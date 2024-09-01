aa3b81579fcd008db511c3e99336a146293ec6e2
Lucas C. Noman <lucas.noman@outlook.com>
2024-09-01 16:19:59 -0300

    Aula_2

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM ubuntu:latest
+
+WORKDIR /app
+
+ENV HOST=localhost PORT=5432
+ENV USER=root PASSWORD=root DBNAME=root
+
+COPY ./main main
+
+EXPOSE 8000
+
+CMD [ "./main" ]
--- database/db.go
+++ database/db.go
@@ -2,7 +2,8 @@ package database
 
 import (
 	"log"
-
+	"os"
+	"fmt"
 	"github.com/guilhermeonrails/api-go-gin/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -14,7 +15,14 @@ var (
 )
 
 func ConectaComBancoDeDados() {
-	stringDeConexao := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
+	stringDeConexao := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("HOST"),
+		os.Getenv("USER"),
+		os.Getenv("PASSWORD"),
+		os.Getenv("DBNAME"),
+		os.Getenv("PORT"),
+	)
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
